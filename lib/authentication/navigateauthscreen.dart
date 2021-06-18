8b4f964a7f1b5d86b57a45d470ba282b7a2dc1ac
Paras Bansal <parasbansal685@gmail.com>
2021-06-18 07:35:23 +0530

    UI Changes

--- lib/authentication/navigateauthscreen.dart
+++ lib/authentication/navigateauthscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:microteams/variables.dart';
 
 class NavigateAuthScreen extends StatefulWidget {
   const NavigateAuthScreen({ Key? key }) : super(key: key);
@@ -10,6 +11,7 @@ class NavigateAuthScreen extends StatefulWidget {
 class _NavigateAuthScreenState extends State<NavigateAuthScreen> {
   @override
   Widget build(BuildContext context) {
+    Color myHexColor = Color(0xffffffff);
     return Scaffold(
       backgroundColor: Colors.grey[250],
       body: Stack(
@@ -21,8 +23,8 @@ class _NavigateAuthScreenState extends State<NavigateAuthScreen> {
               gradient: LinearGradient(
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
-                colors: [Colors.teal, Colors.blue]
-              )
+                colors: [myHexColor, Colors.blueGrey]
+              ),
             ),
             child: Center(
               child: Image.asset(
@@ -30,7 +32,74 @@ class _NavigateAuthScreenState extends State<NavigateAuthScreen> {
                 height: 100,
               ),
             ),
-          )
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 1.6,
+              margin: EdgeInsets.only(left:30, right:30),
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: const Offset(0,3),
+                  ),
+                ],
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20)
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width/2,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Colors.greenAccent, Colors.green]
+                      ),
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "SIGN IN", 
+                        style: mystyle(25, Colors.white),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width/2,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [Colors.pinkAccent, Colors.pink]
+                      ),
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Center(
+                      child: Text(
+                        "SIGN UP", 
+                        style: mystyle(25, Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
--- lib/screens/introauthscreen.dart
+++ lib/screens/introauthscreen.dart
@@ -18,18 +18,18 @@ class _IntroAuthScreenState extends State<IntroAuthScreen> {
           title: "Welcome",
           body: "Welcome to MicroTeams, the best video conference app",
           image: Center(
-            child: Image.asset('images/welcome.png', height: 175 ),
+            child: Image.asset('images/welcome.png', height: 200 ),
           ),
           decoration: PageDecoration(
             bodyTextStyle: mystyle(20,Colors.black),
-            titleTextStyle: mystyle(25,Colors.black)
+            titleTextStyle: mystyle(25,Colors.black),
           )
         ),
         PageViewModel(
           title: "Join or Start meetings",
           body: "Easy to use interface, join or start meetings in a fast time",
           image: Center(
-            child: Image.asset('images/conference.png', height: 175 ),
+            child: Image.asset('images/conference.png', height: 250 ),
           ),
           decoration: PageDecoration(
             bodyTextStyle: mystyle(20,Colors.black),
@@ -40,7 +40,7 @@ class _IntroAuthScreenState extends State<IntroAuthScreen> {
           title: "Security",
           body: "Your security is important for us. Our servers are secure and reliable",
           image: Center(
-            child: Image.asset('images/secure.jpg', height: 175 ),
+            child: Image.asset('images/secure.png', height: 280 ),
           ),
           decoration: PageDecoration(
             bodyTextStyle: mystyle(20,Colors.black),
@@ -48,17 +48,35 @@ class _IntroAuthScreenState extends State<IntroAuthScreen> {
           )
         )
       ],
+      color: Colors.blue,
       onDone: (){
         Navigator.push(context, MaterialPageRoute(builder: (context)=>NavigateAuthScreen()));
       },
       onSkip: (){},
       showSkipButton: true,
-      skip: const Icon(Icons.skip_next, size: 45),
-      next: const Icon(Icons.arrow_forward),
+      skip: Text(
+        "Skip", 
+        style: mystyle(20,Colors.black),
+      ),
+      next: const Icon(
+        Icons.arrow_forward, 
+        size: 30, 
+        color: Colors.black,
+      ),
       done: Text(
         "Done", 
         style: mystyle(20,Colors.black),
       ),
+      dotsDecorator: DotsDecorator(
+        size: const Size.square(10.0),
+        activeSize: const Size(20.0, 10.0),
+        activeColor: Colors.black,
+        color: Colors.black26,
+        spacing: const EdgeInsets.symmetric(horizontal: 5.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0)
+        )
+      )
     );
   }
 }
